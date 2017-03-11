e21f0c86155c32736cf2299b6697d4d0bf773c3f
Vyacheslav Krachkov <vskrachkov@gmail.com>
2017-03-11 21:38:55 +0200

    added wc anologue and simbol counter programs

---
+++ simple_io/10_wc_analogue.c
@@ -0,0 +1,48 @@
+/*
+ * Program that counts number of lines, spaces and words.
+ */
+
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+
+int is_new_word(prev) {
+	if (prev == IN)
+		return 1;
+	return 0;
+}
+
+
+int main() {
+	int c, sp, tb, nl, w, prev;
+	sp = tb = nl = w = 0;
+
+	while ((c = getchar()) != EOF) {
+		if (c == '\n') {
+			++nl;
+			w += is_new_word(prev);
+			prev = OUT;
+		} else if (c == '\t') {
+			++tb;
+			w += is_new_word(prev);
+			prev = OUT;
+		} else if (c == ' ') {
+			++sp;
+			w += is_new_word(prev);
+			prev = OUT;
+		} else {
+			prev = IN;
+		}
+	}
+	printf("\n\n"
+	       "Results:\n"
+		   "--------\n"
+		   "Lines: %d\n"
+		   "Spaces: %d\n"
+		   "Tabs: %d\n"
+		   "Words: %d\n",
+		   nl, sp, tb, w);
+	return 0;
+}
---
+++ simple_io/11_simbol_counter.c
@@ -0,0 +1,30 @@
+/*
+ * Program counts number of passed simbols (digits, delimiters and other).
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	int c, i, del, other;
+	del = other = 0;
+	// array for storing count of each digit which passed in command prompt.
+	int digits[10];
+	// initializing array.
+	for (i = 0; i < 10; ++i)
+		digits[i] = 0;
+	while ( (c = getchar()) != EOF ) {
+		if (c >= '0' && c <= '9')
+			++digits[c - '0'];
+		else if ( c == ' ' || c == '\n' || c == '\t')
+			++del;
+		else
+			++other;
+	}
+	printf("\n\nDigits:\n");
+	for (i = 0; i < 10; ++i)
+		printf("%s%2d%-14s%d\n", "#", i, ": ", digits[i]);
+	printf("%-15s%3d\n", "Delimiters:", del);
+	printf("%-15s%3d\n", "Other:", other);
+	return 0;
+}
